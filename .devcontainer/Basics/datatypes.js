a933e6a3ffe628d735cdf8a44b4f13fbf9dcaf78
AmanMittal2804 <87497830+AmanMittal2804@users.noreply.github.com>
2023-08-30 07:13:19 +0000

    dataTypes

---
+++ .devcontainer/Basics/datatypes.js
@@ -0,0 +1,13 @@
+console.log("Primitive data types")
+// number = 2 to power18
+// string
+// boolean
+// null = standalone value
+// undefined
+// sumbol = unique
+let state = null
+// null associates that no value is been defined (not 0 )
+
+console.log(typeof null); 
+// return object
+
