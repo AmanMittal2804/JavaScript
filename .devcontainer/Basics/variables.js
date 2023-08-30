53932f271d95e7b05764fd75765a588aa60eced9
AmanMittal2804 <87497830+AmanMittal2804@users.noreply.github.com>
2023-08-30 06:59:20 +0000

    letConstVar

---
+++ .devcontainer/Basics/variables.js
@@ -0,0 +1,10 @@
+const accountID= "1765332"
+// Const is the value which will remain same all over the codebase
+
+let emailID= "mittalaman2804@gmail.com"
+// Only the value will be defined inside the block scope 
+
+var accountPassword = "Arup4567"
+// If changes in the value take place it will change all values around the codebase
+
+console.table([accountID,accountPassword,emailID])
