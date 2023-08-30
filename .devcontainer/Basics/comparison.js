31c3972be3722b571d268fc73d0f781414d4c086
AmanMittal2804 <87497830+AmanMittal2804@users.noreply.github.com>
2023-08-30 07:41:48 +0000

    changes

--- .devcontainer/Basics/comparison.js
+++ .devcontainer/Basics/comparison.js
@@ -5,3 +5,8 @@ console.log(null >= 0) //true
 // the equality == and comparison > < <= >= work differently as comparison treats null as 0.
 
 console.log("2" === 2)
+
+const id= Symbol("123")
+const anotherid = Symbol("123")
+
+console.log(id === anotherid) //false
