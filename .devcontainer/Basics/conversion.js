377b4f8a5e184443c550b673cc9ae97c349a74d6
AmanMittal2804 <87497830+AmanMittal2804@users.noreply.github.com>
2023-08-30 07:22:00 +0000

    Convert

---
+++ .devcontainer/Basics/conversion.js
@@ -0,0 +1,11 @@
+let score="abc"
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+// Number is used for the conversion
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// abc => NaN
+// "33"=>33
+// true = 1
