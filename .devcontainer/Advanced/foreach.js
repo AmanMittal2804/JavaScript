bf1332814ab4a7197679b882289ae6b7d58ee11d
AmanMittal2804 <87497830+AmanMittal2804@users.noreply.github.com>
2023-09-05 12:00:19 +0000

    DOM

--- .devcontainer/Advanced/foreach.js
+++ .devcontainer/Advanced/foreach.js
@@ -5,4 +5,9 @@ coding.forEach((item,index,arr) => {
 });
 
 const myNums = [1,2,3,4,5,6,7,8,9,10]
-const 
+const newNums = myNums.filter((num)=>{
+   return num>4 // when {} is there then return keyword is used
+})
+
+console.log(newNums);
+
